c0b31df5f9f36fbb6779773e0719995f73327e69
njfranck <njfranck@ca20c521.ugent.be>
2013-11-21 14:14:35 +0100

    auto reconnect only for mysql

--- lib/Catmandu/Store/DBI.pm
+++ lib/Catmandu/Store/DBI.pm
@@ -1,9 +1,8 @@
 package Catmandu::Store::DBI;
-
 use namespace::clean;
 use Catmandu::Sane;
 
-our $VERSION = "0.02";
+our $VERSION = "0.03";
 
 use DBI;
 use Moo;
@@ -15,51 +14,51 @@ has username    => (is => 'ro', default => sub { '' });
 has password    => (is => 'ro', default => sub { '' });
 
 has dbh => (
-    is       => 'ro',
-    init_arg => undef,
-    lazy     => 1,
-    builder  => '_build_dbh',
+  is       => 'ro',
+  init_arg => undef,
+  lazy     => 1,
+  builder  => '_build_dbh',
 );
-
+#TODO: only mysql seems to need auto_reconnect for now. Postgres and sqlite do not end sessions.
 sub _build_dbh {
-    my $self = $_[0];
-    my $opts = {
-        AutoCommit => 1,
-        RaiseError => 1,
-        mysql_auto_reconnect => 1,
-    };
-    DBI->connect($self->data_source, $self->username, $self->password, $opts);
+  my $self = $_[0];
+  my $opts = {
+    AutoCommit => 1,
+    RaiseError => 1,
+    mysql_auto_reconnect => 1,
+  };
+  DBI->connect($self->data_source, $self->username, $self->password, $opts);
 }
 
 sub transaction {
-    my ($self, $sub) = @_;
+  my ($self, $sub) = @_;
 
-    if ($self->{_tx}) {
-        return $sub->();
-    }
+  if($self->{_tx}){
+    return $sub->();
+  }
 
-    my $dbh = $self->dbh;
-    my @res;
+  my $dbh = $self->dbh;
+  my @res;
 
-    eval {
-        $self->{_tx} = 1;
-        $dbh->begin_work;
-        @res = $sub->();
-        $dbh->commit;
-        $self->{_tx} = 0;
-        1;
-    } or do {
-        my $err = $@;
-        eval { $dbh->rollback };
-        $self->{_tx} = 0;
-        die $err;
-    };
+  eval {
+    $self->{_tx} = 1;
+    $dbh->begin_work;
+    @res = $sub->();
+    $dbh->commit;
+    $self->{_tx} = 0;
+    1;
+  } or do {
+    my $err = $@;
+    eval { $dbh->rollback };
+    $self->{_tx} = 0;
+    die $err;
+  };
 
-    @res;
+  @res;
 }
 
 sub DEMOLISH {
-    $_[0]->{dbh}->disconnect if $_[0]->{dbh};
+  $_[0]->dbh->disconnect if $_[0]->dbh;
 }
 
 package Catmandu::Store::DBI::Bag;
@@ -82,92 +81,93 @@ sub BUILD {
 }
 
 sub _build_sql_get {
-    my $name = $_[0]->name; "select data from $name where id=?";
+  my $name = $_[0]->name; "select data from $name where id=?";
 }
 
 sub _build_sql_delete {
-    my $name = $_[0]->name; "delete from $name where id=?";
+  my $name = $_[0]->name; "delete from $name where id=?";
 }
 
 sub _build_sql_delete_all {
-    my $name = $_[0]->name; "delete from $name";
+  my $name = $_[0]->name; "delete from $name";
 }
 
 sub _build_sql_generator {
-    my $name = $_[0]->name; "select data from $name";
+  my $name = $_[0]->name; "select data from $name";
 }
 
 sub _build_sql_count {
-    my $name = $_[0]->name; "select count(*) from $name";
+  my $name = $_[0]->name; "select count(*) from $name";
 }
 
 sub _build_add_sqlite {
-    my $self = $_[0];
-    my $name = $self->name;
+  my $self = $_[0];
+  my $name = $self->name;
+  my $sql  = "insert or replace into $name(id,data) values(?,?)";
+  sub {
     my $dbh  = $self->store->dbh;
-    my $sql  = "insert or replace into $name(id,data) values(?,?)";
-    sub {
-        my $sth = $dbh->prepare_cached($sql) or Catmandu::Error->throw($dbh->errstr);
-        $sth->execute($_[0], $_[1]) or Catmandu::Error->throw($sth->errstr);
-        $sth->finish;
-    };
+    my $sth = $dbh->prepare_cached($sql) or Catmandu::Error->throw($dbh->errstr);
+    $sth->execute($_[0], $_[1]) or Catmandu::Error->throw($sth->errstr);
+    $sth->finish;
+  };
 }
-
+#TODO: oproepen van dbh IN de callback stoppen! (anders werk je met verouderde/dode connectie)
 sub _build_add_mysql {
-    my $self = $_[0];
-    my $name = $self->name;
+  my $self = $_[0];
+  my $name = $self->name;
+  my $sql  = "insert into $name(id,data) values(?,?) on duplicate key update data=values(data)";
+  sub {
     my $dbh  = $self->store->dbh;
-    my $sql  = "insert into $name(id,data) values(?,?) on duplicate key update data=values(data)";
-    sub {
-        my $sth = $dbh->prepare_cached($sql) or Catmandu::Error->throw($dbh->errstr);
-        $sth->execute($_[0], $_[1]) or Catmandu::Error->throw($sth->errstr);
-        $sth->finish;
-    };
+    my $sth = $dbh->prepare_cached($sql) or Catmandu::Error->throw($dbh->errstr);
+    $sth->execute($_[0], $_[1]) or Catmandu::Error->throw($sth->errstr);
+    $sth->finish;
+  };
 }
 sub _build_add_postgres {
+
   #see: http://stackoverflow.com/questions/15840922/where-not-exists-in-postgresql-gives-syntax-error
   my $self = $_[0];
   my $name = $self->name;
-  my $dbh  = $self->store->dbh;
   my $sql_update = "update $name set data=? where id=?";
   my $sql_insert = "insert into $name select ?,? where not exists (select 1 from $name where id=?)";
   sub {
-      my $sth = $dbh->prepare_cached($sql_update) or Catmandu::Error->throw($dbh->errstr);
-      $sth->execute($_[1], $_[0]) or Catmandu::Error->throw($sth->errstr);
-      unless ($sth->rows) {
-          $sth->finish;
-          $sth = $dbh->prepare_cached($sql_insert) or Catmandu::Error->throw($dbh->errstr);
-          $sth->execute($_[0], $_[1], $_[0]) or Catmandu::Error->throw($sth->errstr);
-          $sth->finish;
-      }
+    my $dbh  = $self->store->dbh;
+    my $sth = $dbh->prepare_cached($sql_update) or Catmandu::Error->throw($dbh->errstr);
+    $sth->execute($_[1], $_[0]) or Catmandu::Error->throw($sth->errstr);
+    unless ($sth->rows) {
+      $sth->finish;
+      $sth = $dbh->prepare_cached($sql_insert) or Catmandu::Error->throw($dbh->errstr);
+      $sth->execute($_[0], $_[1], $_[0]) or Catmandu::Error->throw($sth->errstr);
+      $sth->finish;
+    }
   };
 }
 sub _build_add_generic {
-    my $self = $_[0];
-    my $name = $self->name;
+  my $self = $_[0];
+  my $name = $self->name;
+  my $sql_update = "update $name set data=? where id=?";
+  my $sql_insert = "insert into $name values(?,?) where not exists (select 1 from $name where id=?)";
+  sub {
     my $dbh  = $self->store->dbh;
-    my $sql_update = "update $name set data=? where id=?";
-    my $sql_insert = "insert into $name values(?,?) where not exists (select 1 from $name where id=?)";
-    sub {
-        my $sth = $dbh->prepare_cached($sql_update) or Catmandu::Error->throw($dbh->errstr);
-        $sth->execute($_[1], $_[0]) or Catmandu::Error->throw($sth->errstr);
-        unless ($sth->rows) {
-            $sth->finish;
-            $sth = $dbh->prepare_cached($sql_insert) or Catmandu::Error->throw($dbh->errstr);
-            $sth->execute($_[0], $_[1], $_[0]) or Catmandu::Error->throw($sth->errstr);
-            $sth->finish;
-        }
-    };
+    my $sth = $dbh->prepare_cached($sql_update) or Catmandu::Error->throw($dbh->errstr);
+    $sth->execute($_[1], $_[0]) or Catmandu::Error->throw($sth->errstr);
+    unless ($sth->rows) {
+      $sth->finish;
+      $sth = $dbh->prepare_cached($sql_insert) or Catmandu::Error->throw($dbh->errstr);
+      $sth->execute($_[0], $_[1], $_[0]) or Catmandu::Error->throw($sth->errstr);
+      $sth->finish;
+    }
+  };
 }
 
 sub _build_create {
-  my $self = $_[0];
+  my $self = $_[0];  
   my $driver_name = $self->store->dbh->{Driver}{Name} // "";
   if($driver_name =~ /pg/i){ return $self->_build_create_postgres }
   $self->_build_create_generic;
 }
 sub _build_create_postgres {
-  my $self = $_[0];
+  my $self = $_[0];  
   my $name = $self->name;
   my $dbh  = $self->store->dbh;
   #requires Postgres 9.1
@@ -186,74 +186,74 @@ sub _build_create_generic {
 
 
 sub _build_add {
-    my $self = $_[0];
-    my $driver_name = $self->store->dbh->{Driver}{Name} // "";
-    if ($driver_name =~ /sqlite/i) { return $self->_build_add_sqlite; }
-    if ($driver_name =~ /mysql/i)  { return $self->_build_add_mysql; }
-    if($driver_name =~ /pg/i){ return $self->_build_add_postgres; }
-    return $self->_build_add_generic;
+  my $self = $_[0];
+  my $driver_name = $self->store->dbh->{Driver}{Name} // "";
+  if ($driver_name =~ /sqlite/i) { return $self->_build_add_sqlite; }
+  if ($driver_name =~ /mysql/i)  { return $self->_build_add_mysql; }
+  if($driver_name =~ /pg/i){ return $self->_build_add_postgres; }
+  return $self->_build_add_generic;
 }
 
 sub get {
-    my ($self, $id) = @_;
-    my $dbh = $self->store->dbh;
-    my $sth = $dbh->prepare_cached($self->_sql_get) or Catmandu::Error->throw($dbh->errstr);
-    $sth->execute($id) or Catmandu::Error->throw($sth->errstr);
-    my $data;
-    if (my $row = $sth->fetchrow_arrayref) {
-        $data = $self->deserialize($row->[0]);
-    }
-    $sth->finish;
-    $data;
+  my($self,$id)=@_;
+  my $dbh = $self->store->dbh;
+  my $sth = $dbh->prepare_cached($self->_sql_get) or Catmandu::Error->throw($dbh->errstr);
+  $sth->execute($id) or Catmandu::Error->throw($sth->errstr);
+  my $data;
+  if(my $row = $sth->fetchrow_arrayref) {
+    $data = $self->deserialize($row->[0]);
+  }
+  $sth->finish;
+  $data;
 }
 
 sub add {
-    my ($self, $data) = @_;
-    $self->_add->($data->{_id}, $self->serialize($data));
+  my($self,$data)=@_;
+  $self->_add->($data->{_id},$self->serialize($data));
 }
 
 sub delete_all {
-    my ($self) = @_;
-    my $dbh = $self->store->dbh;
-    my $sth = $dbh->prepare_cached($self->_sql_delete_all) or Catmandu::Error->throw($dbh->errstr);
-    $sth->execute or Catmandu::Error->throw($sth->errstr);
-    $sth->finish;
+  my($self)=@_;
+  my $dbh = $self->store->dbh;
+  my $sth = $dbh->prepare_cached($self->_sql_delete_all) or Catmandu::Error->throw($dbh->errstr);
+  $sth->execute or Catmandu::Error->throw($sth->errstr);
+  $sth->finish;
 }
 
 sub delete {
-    my ($self, $id) = @_;
-    my $dbh = $self->store->dbh;
-    my $sth = $dbh->prepare_cached($self->_sql_delete) or Catmandu::Error->throw($dbh->errstr);
-    $sth->execute($id) or Catmandu::Error->throw($sth->errstr);
-    $sth->finish;
+  my($self,$id)= @_;
+  my $dbh = $self->store->dbh;
+  my $sth = $dbh->prepare_cached($self->_sql_delete) or Catmandu::Error->throw($dbh->errstr);
+  $sth->execute($id) or Catmandu::Error->throw($sth->errstr);
+  $sth->finish;
 }
 
 sub generator {
-    my ($self) = @_;
-    my $dbh = $self->store->dbh;
-    sub {
-        state $sth;
-        state $row;
-        unless ($sth) {
-            $sth = $dbh->prepare($self->_sql_generator) or Catmandu::Error->throw($dbh->errstr);
-            $sth->execute;
-        }
-        if ($row = $sth->fetchrow_arrayref) {
-            return $self->deserialize($row->[0]);
-        }
-        $sth->finish;
-        return;
-    };
+  my($self)=@_;
+  my $dbh = $self->store->dbh;
+  sub {
+    state $sth;
+    state $row;
+    unless ($sth) {
+      $sth = $dbh->prepare($self->_sql_generator) or Catmandu::Error->throw($dbh->errstr);
+      $sth->execute;
+    }
+    if($row = $sth->fetchrow_arrayref){
+      return $self->deserialize($row->[0]);
+    }
+    $sth->finish;
+    return;
+  };
 }
 
 sub count {
-    my ($self) = @_;
-    my $dbh = $self->store->dbh;
-    my $sth = $dbh->prepare_cached($self->_sql_count) or Catmandu::Error->throw($dbh->errstr);
-    $sth->execute or Catmandu::Error->throw($sth->errstr);
-    my ($n) = $sth->fetchrow_array;
-    $sth->finish;
-    $n;
+  my($self)= @_;
+  my $dbh = $self->store->dbh;
+  my $sth = $dbh->prepare_cached($self->_sql_count) or Catmandu::Error->throw($dbh->errstr);
+  $sth->execute or Catmandu::Error->throw($sth->errstr);
+  my ($n) = $sth->fetchrow_array;
+  $sth->finish;
+  $n;
 }
 
 1;
@@ -316,6 +316,10 @@ Nicolas Steenlant, C<< <nicolas.steenlant at ugent.be> >>
 =head1 CONTRIBUTOR
 
 Vitali Peil C<< <vitali.peil at uni-bielefeld.de> >>
+
+=head1 CONTRIBUTOR
+
+Nicolas Franck C<< <nicolas.franck at ugent.be> >>
 
 =head1 SEE ALSO
 
